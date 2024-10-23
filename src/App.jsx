cf0d1b31df2bde73c18a5c50b163355a78ca7764
Marinaniazi786 <marinahkniazi@gmail.com>
2024-10-23 14:04:21 +0500

    image did comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
-import OIP from "./assets/OIP.jpg";
+// import OIP from "./assets/OIP.jpg";
 
 function App() {
   const [count, setCount] = useState(0);
